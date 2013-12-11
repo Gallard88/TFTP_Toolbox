a0e6aa5b58fa50db04a9b691f8309173c228f1cc
Thomas Burns <ThomasBurns88@gmail.com>
2013-12-12 07:26:08 +1100

    Added Licence info to file

--- TFTP_Server.c
+++ TFTP_Server.c
@@ -1,5 +1,25 @@
+/*
+ TFTP Server ( http://www.github.com/Gallard88/TFTP_Toolbox )
+ Copyright (c) 2013 Thomas BURNS
 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
 
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+*/
 // *******************************************************************************************
 
 #include <stdio.h>
@@ -97,7 +117,8 @@ void TFTP_Send_Error(int sock, int error_type, struct sockaddr_storage *address)
 
 // *******************************************************************************************
 static void Correct_Path(char *path)
-{// if it sees a windows style path name, correct it and turn it into a unix style.
+{
+  // if it sees a windows style path name, correct it and turn it into a unix style.
   while ( *path != 0 ) {
     if ( *path == '\\') {
       *path = '/';
